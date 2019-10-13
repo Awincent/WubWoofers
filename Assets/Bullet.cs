dd3790a003d005eae3a6d8f679a7b80aabdbc51d
Wade <assar41@gmail.com>
2019-10-13 22:02:15 +0200

    Last push for today

---
+++ Assets/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    GameObject bullet;
+    public Rigidbody rigidbody;
+    public float speed;
+
+    void Start()
+    {
+        bullet = this.gameObject;
+    }
+
+
+    void Update()
+    {
+        rigidbody.velocity = transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }   
+    }
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        } 
+    }
+}
