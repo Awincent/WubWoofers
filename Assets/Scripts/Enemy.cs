674f97a6b29873158029eb066aa77c25cbd56f50
Wade <assar41@gmail.com>
2019-10-14 09:44:24 +0200

    oh no

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    int lives;
+    float speed;
+
+    public void EnemyDeath()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
