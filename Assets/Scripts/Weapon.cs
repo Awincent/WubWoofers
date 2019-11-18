cc158a55ce31d98b55840af7a0c7f35112f689d8
NTIGVirtual <57398779+NTIGVirtual@users.noreply.github.com>
2019-11-18 12:29:53 +0100

    Plank

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapo : MonoBehaviour
+{
+    public GameObject leftController;
+    public GameObject rightController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
