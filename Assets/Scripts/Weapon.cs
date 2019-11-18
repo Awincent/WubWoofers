b0512cee1fead12b1d72517be3ff053cdf7f8105
TheVolcanicSeal <oskar.emanuelsson@hotmail.se>
2019-11-18 12:46:32 +0100

    proximityTest

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class weapo : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
+
+    public float grabDistance;
     public GameObject leftController;
     public GameObject rightController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,14 @@ public class weapo : MonoBehaviour
     void Update()
     {
         
+        if(Vector3.Distance(transform.position, leftController.transform.position) < grabDistance || Vector3.Distance(transform.position, rightController.transform.position) < grabDistance)
+        {
+
+            GetComponent<Renderer>().material.color = Color.red;
+
+        }
+
     }
+    
+
 }
