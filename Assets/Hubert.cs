a4471c4ada2c3bff093a2753a0cc399a3a81bf14
Wade <assar41@gmail.com>
2019-12-06 11:58:10 +0100

    yeet boi

---
+++ Assets/Hubert.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hubert : MonoBehaviour
+{
+    GameObject hubert;
+    Vector3 vector = new Vector3(.05f,0,.05f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hubert = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hubert.transform.position += vector;
+    }
+}
