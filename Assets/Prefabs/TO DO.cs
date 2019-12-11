141b9c0e4c995f9a33014e9ae2a7ca597a7a57c8
Wade <assar41@gmail.com>
2019-12-11 14:57:21 +0100

    pitto anim fixed mm

---
+++ Assets/Prefabs/TO DO.cs
@@ -0,0 +1,12 @@
+﻿// Pitto remake
+// Throwables
+// Gatto sound + rockets
+// Gatto anim
+// World remake and glowup
+// Pitto anim
+// Gatto particles
+// Rocket particles
+// Mini rocket particles
+// Spawn waves (base system) (configs and waves?)
+// World colliders
+// Mini rocket system
